bd8901d82797c4e0e7cd6f5174e35a1e91cd3ef0
Logen <79722764+btcforplebs@users.noreply.github.com>
2025-09-08 19:46:57 -0400

    gitignore

--- build/lib/main.py
+++ build/lib/main.py
@@ -71,8 +71,8 @@ def main():
         print(f"Error: {e}", file=sys.stderr)
         sys.exit(1)
 
-    print("NSEc:", nsec)
-    print("NPop:", npub)
+    print("NSec:", nsec)
+    print("NPub:", npub)
 
 
 if __name__ == "__main__":
